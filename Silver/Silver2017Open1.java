9c9e81a3c4a15dcb9f0016e8b11e30684c00eef2
nivek-bori <134992572+nivek-bori@users.noreply.github.com>
2024-07-31 20:49:35 -0700

    Update Silver2017Open1.java

--- Silver/Silver2017Open1.java
+++ Silver/Silver2017Open1.java
@@ -1,3 +1,5 @@
+// First Solution
+
 import java.io.*;
 import java.util.*;
 
@@ -53,5 +55,72 @@ public class Main {
       out.println(maxTime);
       in.close();
       out.close();
+   }
+}
+
+// Second Solution
+//Silver2017Open1.java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   public static void main(String[] args) throws IOException {
+      BufferedReader in = new BufferedReader(new FileReader("pairup.in"));
+      PrintWriter out = new PrintWriter(new FileWriter("pairup.out"));
+
+      int n = Integer.parseInt(in.readLine());
+
+      Cows[] cows = new Cows[n];
+      for (int i = 0; i < n; i++) {
+         String[] strCow = in.readLine().split(" ");
+         cows[i] = new Cows(Integer.parseInt(strCow[0]), Integer.parseInt(strCow[1]));
+      }
+
+      Arrays.sort(cows, new CowComparer());
+
+      int maxTime = 0;
+      int i = 0;
+      int j = n - 1;
+      while (i < j) {
+         Cows cowA = cows[i];
+         Cows cowB = cows[j];
+         maxTime = Math.max(cowA.t + cowB.t, maxTime);
+         if (cowA.num < cowB.num) {
+            cowB.num -= cowA.num;
+            i++;
+         } else if (cowB.num < cowA.num) {
+            cowA.num -= cowB.num;
+            j--;
+         } else {
+            i++;
+            j--;
+         }
+      }
+
+      if (i == j && cows[i].num > 0) {
+         maxTime = Math.max(2 * cows[i].num, maxTime);
+      }
+
+      out.println(maxTime);
+      in.close();
+      out.close();
+   }
+
+   public static class Cows {
+      public int num;
+      public int t;
+
+      public Cows(Integer _num, Integer _t) {
+         num = _num;
+         t = _t;
+      }
+   }
+
+   public static class CowComparer implements Comparator<Cows> {
+      @Override
+      public int compare(Cows a, Cows b) {
+         return Integer.compare(a.t, b.t);
+      }
    }
 }
