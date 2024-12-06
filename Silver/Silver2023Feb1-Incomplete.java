41626fba86ce04992c0d1f12c4447f8b5f0bda8f
nivek-bori <134992572+nivek-bori@users.noreply.github.com>
2024-12-06 08:39:54 -0800

    Update Silver2023Feb1-Incomplete.java

--- Silver/Silver2023Feb1-Incomplete.java
+++ Silver/Silver2023Feb1-Incomplete.java
@@ -2,20 +2,66 @@ binary search cost range is: 0 - (timeC + timeM)
 given a cost, binary search money on cookies
   save state with 1000 * c + m
 
-int timeC = Input;
-int timeM = Input;
+public class Main {
+    private static int a;
+    private static int b;
+    
+    private static int[] amountA;
+    private static int[] amountB;
+    private static int[] maxT;
+    
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
 
-int lo = 0;
-int hi = timeC + timeM;
+        amountA = new int[n];
+        amountB = new int[n];
+        maxT = new int[n];
 
-while (lo < hi) {
-  int mid = (int) ((hi + lo + 1) / 2.0);
+        for (int i = 0; i < n; i++) {
+            String[] friendStr = in.readLine().split(" ");
+            amountA[i] = Integer.parseInt(friendStr[0]);
+            amountB[i] = Integer.parseInt(friendStr[1]);
+            maxT[i] = Integer.parseInt(friendStr[2]);
+        }
 
-  if (try(mid)) {
-    lo = mid + 1;
-  } else {
-    hi = mid;
-  }
+        int lo = 0;
+        int hi = 0;
+        while (lo < hi) {
+            int mid = (int) (0.5 * (hi + lo + 1));
+
+            if (tryCost(mid)) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+    }
+
+    public static boolean tryCost(int c) {
+        HashSet<Integer> attempted = new HashSet<>();
+        int A = a - (int) (0.5 * c);
+        int B = b - (c - A);
+        int n = maxT.length;
+
+        while (!attempted.add(1000 * A + B) && A > 1 && B > 1) {
+            for (int i = 0; i < n; i++) {
+                if (amountA[i] * A + amountB[i] * B > maxT[i]) {
+                    if (amountA[i] * A > amountB[i] * B) {
+                        A--;
+                        B++;
+                    } else {
+                        A++;
+                        B--;
+                    }
+                    break;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
 }
-
-return (lo + hi + 1) / 2;
