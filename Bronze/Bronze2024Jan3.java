95b7db2219f495c3d9ad68b68fa4942e6ae11d9e
nivek-bori <134992572+nivek-bori@users.noreply.github.com>
2024-12-09 16:21:12 -0800

    Create Bronze2024Jan3.java

---
+++ Bronze/Bronze2024Jan3.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+//        BufferedReader in = new BufferedReader(new FileReader("src/system.in"));
+//        PrintWriter out = new PrintWriter(new FileWriter("src/system.out"));
+
+        int n = Integer.parseInt(in.readLine());
+
+        String[] fieldStr = in.readLine().split(" ");
+        long sum = 0;
+        long delta = 0;
+        long cnt = 0;
+        for (int i = 0; i < n; i++) {
+            long val = Long.parseLong(fieldStr[i]);
+            sum += delta;
+            cnt += Math.abs(sum + val);
+            delta -= sum + val;
+            sum -= sum + val;
+        }
+
+        out.println(cnt);
+        in.close();
+        out.close();
+    }
+}
