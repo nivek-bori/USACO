0415a3ddfbb44565337385b75f0b0ac6b404fecd
nivek-bori <134992572+nivek-bori@users.noreply.github.com>
2024-12-10 13:56:14 -0800

    Update Bronze2024Open3-Incomplete.java

--- Bronze/Bronze2024Open3-Incomplete.java
+++ Bronze/Bronze2024Open3-Incomplete.java
@@ -1,51 +1,55 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-//        BufferedReader in = new BufferedReader(new FileReader("src/system.in"));
-//        PrintWriter out = new PrintWriter(new FileWriter("src/system.out"));
-
-        int n = Integer.parseInt(in.readLine());
-
-        if (n == 2) {
-            out.println("1 2");
-            return;
+        if (Integer.parseInt(writtenStr[n - 1]) != 1) {
+            return false;
         }
-
-        int countOne = 0;
-        String[] valuesStr = in.readLine().split(" ");
-        int[] values = new int[n - 1];
-
-        if (Integer.parseInt(valuesStr[n - 2]) != 1) {
-            out.println("-1");
-            return;
-        }
-
-        for (int i = 0; i < n - 1; i++) {
-            values[i] = Integer.parseInt(valuesStr[i]);
-            if (values[i] == 1) {
-                if (i < n - 2) {
-                    out.println("-1");
-                    break;
-                }
-                countOne++;
+        
+        if (n == 1) {return true;}
+        
+        int[] written = new int[n];
+        for (int i = 0; i < n; i++) {
+            written[i] = Integer.parseInt(writtenStr[i]);
+            if (written[i] == 1 && i < n - 2) {
+                return false;
             }
         }
-
-        if (countOne == 0 || countOne > 2) {
-            out.println("-1");
-            return;
+        
+        LinkedList<Integer> missing = getMissing(written, n);
+        if (missing.size() > 2) {
+            return false;
         }
-
-        if (countOne == 1) {
-            for (int i = 0; i < n; i++) {
-                out.print(i + " ");
+        if (missing.size() == 1){
+            if (written[n - 2] == 1) {
+                return false;
+            } else {
+                missing.insert(0, 1);
             }
-            out.println();
-        } else {
-
         }
+        
+        int lo = 0;
+        int hi = n - 1;
+        written[0] = mising.get(0);
+        written[n - 1] = missing.get(1);
+        
+        for (int val : written) {
+            if (written[lo] > written[hi]) {
+                lo++;
+                written[lo] = val;
+            } else {
+                hi--;
+                written[hi] = val;
+            }
+        }
+        
+        for (int val : written) {
+            out.print(val + " ");
+        }
+        out.println();
+        
+        in.close();
+        out.close();
     }
 }
