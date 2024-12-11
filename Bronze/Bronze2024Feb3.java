507aa92664541e3eb83d994ad845496e0a0dce76
nivek-bori <134992572+nivek-bori@users.noreply.github.com>
2024-12-10 17:27:22 -0800

    Update and rename Bronze2024Feb3-NotComfirmed.java.java to Bronze2024Feb3.java

---
+++ Bronze/Bronze2024Feb3.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        // BufferedReader in = new BufferedReader(new FileReader("system.in"));
+        // PrintWriter out = new PrintWriter(new FileWriter("system.out"));
+
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        String[] endsStr = in.readLine().split(" ");
+        String[] timesStr = in.readLine().split(" ");
+
+        Integer[] requiredWakeUpTimes = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            requiredWakeUpTimes[i] = Integer.parseInt(endsStr[i]) - Integer.parseInt(timesStr[i]) - 1;
+        }
+        Arrays.sort(requiredWakeUpTimes, Collections.reverseOrder());
+
+        for (int q = 0; q < Q; q++) {
+            st = new StringTokenizer(in.readLine());
+            int requiredCount = Integer.parseInt(st.nextToken());
+            int wakeUp = Integer.parseInt(st.nextToken());
+
+            int lo = 0;
+            int hi = n - 1;
+            while (lo < hi) {
+                int mid = (lo + hi) / 2;
+
+                if (wakeUp <= requiredWakeUpTimes[mid]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid;
+                }
+            }
+
+            if (requiredCount <= lo) {
+                out.println("YES");
+            } else {
+                out.println("NO");
+            }
+        }
+
+        in.close();
+        out.close();
+    }
+}
