0b0bad90a401167522470b2a05916c74e1dc5617
nivek-bori <134992572+nivek-bori@users.noreply.github.com>
2025-01-27 17:39:56 -0800

    Update Bronze3.java

--- Bronze3.java
+++ Bronze3.java
@@ -3,61 +3,128 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
+        boolean testing = false;
 
-        int n = Integer.parseInt(in.readLine());
+        BufferedReader in;
+        PrintWriter out;
 
-        int[] des = new int[n];
-        int[] curr = new int[n];
-
-        int norm = 0;
-
-        StringTokenizer stA = new StringTokenizer(in.readLine());
-        StringTokenizer stB = new StringTokenizer(in.readLine());
-
-        for (int i = 0; i < n; i++) {
-            des[i] = Integer.parseInt(stA.nextToken());
-            curr[i] = Integer.parseInt(stB.nextToken());
-
-            if (des[i] == curr[i]) {norm++;}
+        if (!testing) {
+            in = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(System.out);
+        } else {
+            in = new BufferedReader(new FileReader("system.in"));
+            out = new PrintWriter(new FileWriter("system.out"));
         }
 
-        int[] ret = new int[n + 1];
+        int T = Integer.parseInt(in.readLine());
+        boolean nextTestCase = false;
 
-        for (int c = 0; c < n - 1; c++) {
-            int cntC = norm;
-            int cntO = norm;
-            if (des[c] == curr[c + 1]) {cntO++;}
-            if (des[c + 1] == curr[c]) {cntO++;}
-            if (des[c] == curr[c]) {cntO--;}
-            if (des[c + 1] == curr[c + 1]) {cntO--;}
+        for (int t = 0; t < T; t++) {
+            nextTestCase = false;
 
-            ret[cntO]++;
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            for (int d = 1; d <= Math.min(c, n - c - 2); d++) {
-                int A = c - d;
-                int B = c + d;
+            int[][] req = new int[n][n];
+            ArrayList<Integer> doubles = new ArrayList<>(n / 4);
 
-                if (des[A] == curr[B]) {cntC++;}
-                if (des[B] == curr[A]) {cntC++;}
-                if (des[A] == curr[A]) {cntC--;}
-                if (des[B] == curr[B]) {cntC--;}
-
-                if (des[A] == curr[B + 1]) {cntO++;}
-                if (des[B + 1] == curr[A]) {cntO++;}
-                if (des[A] == curr[A]) {cntO--;}
-                if (des[B + 1] == curr[B + 1]) {cntO--;}
-
-                ret[cntC]++;
-                ret[cntO]++;
+            for (int y = 0; y < n; y++) {
+                String[] line = in.readLine().split("");
+                for (int x = 0; x < n; x++) {
+                    String str = line[x];
+                    if (str.equals("W")) {
+                        req[y][x] = 0;
+                    } else if (str.equals("G")) {
+                        req[y][x] = 1;
+                    } else {
+                        req[y][x] = 2;
+                        doubles.add(n * y + x);
+                    }
+                }
             }
+
+            Collections.sort(doubles, Comparator.reverseOrder());
+
+            boolean[][][] field = new boolean[n][n][2];
+
+            int initStars = 0;
+
+            for (int pos : doubles) {
+                int y = pos / n;
+                int x = pos % n;
+
+                if (y - b < 0 || x - a < 0) {
+                    nextTestCase = true;
+                    break;
+                }
+
+                // Inverse start addition
+                if (!field[y - b][x - a][0]) {
+                    field[y - b][x - a][0] = true;
+                    field[y][x][1] = true;
+                    initStars++;
+
+                    // Requirement check
+                    int cntA = (field[y - b][x - a][0] ? 1 : 0) + (field[y - b][x - a][1] ? 1 : 0);
+                    if (req[y - b][x - a] < cntA) {
+                        nextTestCase = true;
+                        break;
+                    }
+                }
+
+                // Normal star addition
+                if (!field[y][x][0]) {
+                    field[y][x][0] = true;
+                    if (y + b < n && x + a < n) {
+                        int cntB = (field[y + b][x + a][0] ? 1 : 0) + (field[y + b][x + a][1] ? 1 : 0);
+
+                        if (req[y + b][x + a] > cntB) {
+                            field[y + b][x + a][1] = true;
+                        }
+                    }
+                    initStars++;
+                }
+            }
+
+            if (nextTestCase) {
+                out.println(-1);
+                continue;
+            }
+
+            for (int y = 0; y < n; y++) {
+                for (int x = 0; x < n; x++) {
+                    int cnt = (field[y][x][0] ? 1 : 0) + (field[y][x][1] ? 1 : 0);
+
+                    if (req[y][x] > cnt) {
+                        field[y][x][0] = true;
+                        initStars++;
+
+                        if (y + b < n && x + a < n) {
+                            int cntB = (field[y + b][x + a][0] ? 1 : 0) + (field[y + b][x + a][1] ? 1 : 0);
+
+                            if (req[y + b][x + a] > cntB) {
+                                field[y + b][x + a][1] = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (testing) {
+                for (int y = 0; y < n; y++) {
+                    for (int x = 0; x < n; x++) {
+                        out.print((field[y][x][0] ? 1 : 0) + "" + (field[y][x][1] ? 1 : 0) + " ");
+                    }
+                    out.println();
+                }
+            }
+
+            out.println(initStars);
         }
 
-        for (int val : ret) {
-            out.println(val);
-        }
-
-        in.close(); out.close();
+        in.close();
+        out.close();
     }
 }
